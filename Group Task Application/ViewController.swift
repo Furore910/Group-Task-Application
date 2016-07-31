d5b8695767abc551227f2667ea18b38425d3228c
Lachlan <lachlan.ware@sydstu.catholic.edu.au>
2016-07-31 17:39:31 +1000

    Third Commit
    Lachlan joins this contribution. I’m sick

--- Group Task Application/ViewController.swift
+++ Group Task Application/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var school:String = "Primary Children"
+    var Lachlan:String = "I'm Sick!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
