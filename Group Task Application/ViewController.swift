3afb7d89d175d4529a4447055a6e24f8737ac2af
Furore910 <rirb181@gmail.com>
2016-07-31 16:40:25 +1000

    First Commit
    Test Trial to GitHub!

---
+++ Group Task Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Group Task Application
+//
+//  Created by Rian Ruban on 31/07/2016.
+//  Copyright © 2016 Lachlan and Rian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
