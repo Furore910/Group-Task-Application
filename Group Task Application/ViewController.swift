fc3a5f8070e5a69fe47065b952bff2e3989d85bc
Furore910 <rirb181@gmail.com>
2016-08-11 09:50:08 +1000

    Fourth Commit
    Added new buttons to the Storyboard.

--- Group Task Application/ViewController.swift
+++ Group Task Application/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var school:String = "Primary Children"
-    var Lachlan:String = "I'm Sick!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
